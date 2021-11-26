82e304415acfbca01ffe0bde6e1150c2c593c917
luminorena <bakhuta.oa@gmail.com>
2021-11-26 20:30:30 +0300

    rewritten Point to method

---
+++ sandbox/src/main/java/Point/MainClass.java
@@ -0,0 +1,7 @@
+package Point;
+
+public class MainClass {
+    public static void main(String[] args)  {
+       double a = new Point(1,2).calc(new Point(3,4));
+    }
+}
--- sandbox/src/main/java/Point/Point.java
+++ sandbox/src/main/java/Point/Point.java
@@ -5,27 +5,21 @@ import java.lang.Math;
 public class Point {
     public double x;
     public double y;
+    public double rez;
+
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
-
-    public static double distance(Point p1, Point p2) {
-        double rez = Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-       /* можно сделать функцию void, но не выводить sout в main.
-       Задача сделать расчёт, а если тип данных указывать, то тогда
-       появляются лишние строчки кода, что по задаче, в принципе, лишнее
-       System.out.println(rez);
-        */
-       return rez;
-        //System.out.println(rez);
+    public double calc(double rez){
+        return this.rez = rez;
+    }
+    public double calc(Point p) {
+        double rez= Math.sqrt(Math.pow((x - p.x), 2) + Math.pow((y - p.y), 2));
+        System.out.println(rez);
+        return rez;
     }
 
-    public static void main(String[] args) {
-        Point p1 = new Point(2, 2);
-        Point p2 = new Point(1, 3);
-        System.out.println(distance(p1, p2));
-       // distance(p1, p2);
-    }
 }
+
