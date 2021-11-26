9591ccd4082c027b884e9e56479b673669ada608
luminorena <bakhuta.oa@gmail.com>
2021-11-26 20:33:28 +0300

    deleted old test

--- sandbox/src/test/java/Point/PointTests.java
+++ sandbox/src/test/java/Point/PointTests.java
@@ -1,22 +1,5 @@
 package Point;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import static Point.Point.distance;
-@Test
-public class PointTests {
-    public void testPoint(){
-        // нет у теста типа возвращаемого значения, а также функции main
-        Point p1 = new Point(2, 2);
-        Point p2 = new Point(1, 3);
-        double d = distance(p1, p2);
-        // assert для проверки значения, assertEquals также для обработки исключения
-        //assert d == 1.4142135623730951;
-        Assert.assertEquals(d, 1.4142135623730951);
 
 
 
-
-    }
-}
