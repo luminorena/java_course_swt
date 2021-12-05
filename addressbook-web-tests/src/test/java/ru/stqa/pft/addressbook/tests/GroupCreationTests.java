6cf21fad0e11b5b4e17ba944b83d321f3d968938
luminorena <bakhuta.oa@gmail.com>
2021-12-05 17:29:31 +0300

    Groups Deletates

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+    @Test
+    public void testGroupCreation() throws Exception {
+
+        app.gotoGroupPage();
+        app.initGroupCreation();
+        app.fillGroupForm(new GroupData("1", "1", "1"));
+        app.submitGroupCreation();
+        app.returnToGroupPage();
+    }
+
+}
