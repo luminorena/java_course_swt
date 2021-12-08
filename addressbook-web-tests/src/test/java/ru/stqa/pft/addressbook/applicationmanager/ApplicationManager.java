67a0ee22a4a3b1b5f8367a5a37351c7392c082bb
luminorena <bakhuta.oa@gmail.com>
2021-12-08 22:57:55 +0300

    chrome init fixed

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/applicationmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/applicationmanager/ApplicationManager.java
@@ -3,16 +3,11 @@ package ru.stqa.pft.addressbook.applicationmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -34,96 +29,30 @@ public class ApplicationManager {
     public ApplicationManager(String browser) {
         this.browser = browser;
     }
-    //private WebDriver wd;
+
 
     public void init() {
-        if (Objects.equals(browser, BrowserType.FIREFOX)){
+        if (Objects.equals(browser, BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)){
+        } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
         }
-        wd = new WebDriver() {
-            @Override
-            public void get(String url) {
 
-            }
-
-            @Override
-            public String getCurrentUrl() {
-                return null;
-            }
-
-            @Override
-            public String getTitle() {
-                return null;
-            }
-
-            @Override
-            public List<WebElement> findElements(By by) {
-                return null;
-            }
-
-            @Override
-            public WebElement findElement(By by) {
-                return null;
-            }
-
-            @Override
-            public String getPageSource() {
-                return null;
-            }
-
-            @Override
-            public void close() {
-
-            }
-
-            @Override
-            public void quit() {
-
-            }
-
-            @Override
-            public Set<String> getWindowHandles() {
-                return null;
-            }
-
-            @Override
-            public String getWindowHandle() {
-                return null;
-            }
-
-            @Override
-            public TargetLocator switchTo() {
-                return null;
-            }
-
-            @Override
-            public Navigation navigate() {
-                return null;
-            }
-
-            @Override
-            public Options manage() {
-                return null;
-            }
-        };
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
-        contactHelper = new ContactHelper (wd);
+        contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         sessionHelper.login("admin", "secret");
     }
+
     public void stop() {
         wd.quit();
     }
 
     public void logout() {
-       wd.findElement(By.linkText("Logout")).click();
+        wd.findElement(By.linkText("Logout")).click();
     }
 
     public void login(String username, String password) {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupModificationTests extends TestBase{
