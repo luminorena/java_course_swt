81e4c38344479cadbf5b4199c96be3a17c45ae3f
luminorena <bakhuta.oa@gmail.com>
2021-12-08 22:49:12 +0300

    chrome init

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/applicationmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/applicationmanager/ApplicationManager.java
@@ -2,28 +2,114 @@ package ru.stqa.pft.addressbook.applicationmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private GroupHelper groupHelper;
+    private String browser;
 
-    public ApplicationManager(FirefoxDriver wd) {
+    public ApplicationManager(WebDriver wd) {
         navigationHelper = new NavigationHelper(wd);
     }
 
     public ApplicationManager() {
 
     }
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
     //private WebDriver wd;
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (Objects.equals(browser, BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+        wd = new WebDriver() {
+            @Override
+            public void get(String url) {
+
+            }
+
+            @Override
+            public String getCurrentUrl() {
+                return null;
+            }
+
+            @Override
+            public String getTitle() {
+                return null;
+            }
+
+            @Override
+            public List<WebElement> findElements(By by) {
+                return null;
+            }
+
+            @Override
+            public WebElement findElement(By by) {
+                return null;
+            }
+
+            @Override
+            public String getPageSource() {
+                return null;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public void quit() {
+
+            }
+
+            @Override
+            public Set<String> getWindowHandles() {
+                return null;
+            }
+
+            @Override
+            public String getWindowHandle() {
+                return null;
+            }
+
+            @Override
+            public TargetLocator switchTo() {
+                return null;
+            }
+
+            @Override
+            public Navigation navigate() {
+                return null;
+            }
+
+            @Override
+            public Options manage() {
+                return null;
+            }
+        };
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
@@ -32,9 +118,6 @@ public class ApplicationManager {
         sessionHelper = new SessionHelper(wd);
         sessionHelper.login("admin", "secret");
     }
-
-
-
     public void stop() {
         wd.quit();
     }
