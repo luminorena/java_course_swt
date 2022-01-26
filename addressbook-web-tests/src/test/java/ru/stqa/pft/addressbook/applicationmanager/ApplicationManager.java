172bcad89f7164153480236ec585af380fe1fc13
luminorena <bakhuta.oa@gmail.com>
2022-01-26 22:25:46 +0300

    System.properties

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/applicationmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/applicationmanager/ApplicationManager.java
@@ -7,16 +7,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private Properties properties;
     WebDriver wd;
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private GroupHelper groupHelper;
     private String browser;
+
 
     public ApplicationManager(WebDriver wd) {
         navigationHelper = new NavigationHelper(wd);
@@ -26,12 +33,16 @@ public class ApplicationManager {
 
     }
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser)  {
         this.browser = browser;
+        properties = new Properties();
+
     }
 
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader((new File(String.format("src/test/resources/%s.properties", target)))));
         if (Objects.equals(browser, BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -39,12 +50,13 @@ public class ApplicationManager {
         }
 
         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+
     }
 
     public void stop() {
@@ -56,7 +68,7 @@ public class ApplicationManager {
     }
 
     public void login(String username, String password) {
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).clear();
