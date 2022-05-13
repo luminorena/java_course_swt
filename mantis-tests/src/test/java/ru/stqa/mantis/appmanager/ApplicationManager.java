b1be35ebee33674115196200af74eba7aae8c0e7
luminorena <bakhuta.oa@gmail.com>
2022-05-13 18:45:20 +0300

    Lection 8.4 Lazy initialization of helpers

--- mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package ru.stqa.mantis.appmanager;
 
+import javafx.animation.AnimationTimer;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,35 +15,29 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     private Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
+    private   RegistrationHelper registrationHelper;
 
 
     public ApplicationManager(String browser)  {
         this.browser = browser;
         properties = new Properties();
-
     }
-
 
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader((new File(String.format
                 ("src/test/resources/%s.properties", target)))));
-        if (Objects.equals(browser, BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else wd = new ChromeDriver();
 
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
 
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
     public HttpSession newSession(){
@@ -51,5 +46,26 @@ public class ApplicationManager {
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null){
+            if (Objects.equals(browser, BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else wd = new ChromeDriver();
+
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
     }
 }
