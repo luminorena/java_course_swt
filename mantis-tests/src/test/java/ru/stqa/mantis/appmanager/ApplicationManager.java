ac52436e35c51239d5b1444d9024b8ef2843b9f7
luminorena <bakhuta.oa@gmail.com>
2022-05-13 22:48:09 +0300

    8.5 lection Data transfer to remote host using FTP protocol

--- mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
@@ -17,8 +17,10 @@ public class ApplicationManager {
     private Properties properties;
     private WebDriver wd;
 
+
     private String browser;
     private   RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
 
     public ApplicationManager(String browser)  {
@@ -55,6 +57,12 @@ public class ApplicationManager {
         return registrationHelper;
     }
 
+    public FtpHelper ftp(){
+        if (ftp == null){
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
     public WebDriver getDriver() {
         if (wd == null){
             if (Objects.equals(browser, BrowserType.FIREFOX)) {
