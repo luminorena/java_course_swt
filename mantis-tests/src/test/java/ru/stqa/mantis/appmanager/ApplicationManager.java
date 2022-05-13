5d2934981790f01c1bd91ffe73d79363022a8b6d
luminorena <bakhuta.oa@gmail.com>
2022-05-13 18:14:29 +0300

    lection 8.3 fix

--- mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
@@ -36,7 +36,7 @@ public class ApplicationManager {
             wd = new ChromeDriver();
         } else wd = new ChromeDriver();
 
-        wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
 
     }
--- mantis-tests/src/test/java/ru/stqa/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/appmanager/HttpSession.java
@@ -33,7 +33,8 @@ public class HttpSession {
         post.setEntity(new UrlEncodedFormEntity(params));
         CloseableHttpResponse response = httpClient.execute(post);
         String body = geTextFrom(response);
-        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+        return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
+
     }
 
     private String geTextFrom(CloseableHttpResponse response) throws IOException {
@@ -48,7 +49,7 @@ public class HttpSession {
         HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
         CloseableHttpResponse response = httpClient.execute(get);
         String body = geTextFrom(response);
-        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+        return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
 
     }
 }
