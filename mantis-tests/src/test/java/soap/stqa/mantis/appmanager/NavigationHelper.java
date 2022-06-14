ee3b90de39805de1dcc4a71a1a94fe11043eecd5
luminorena <bakhuta.oa@gmail.com>
2022-06-14 13:25:44 +0300

    task 18 selenium changed and locator

--- mantis-tests/src/test/java/soap/stqa/mantis/appmanager/NavigationHelper.java
+++ mantis-tests/src/test/java/soap/stqa/mantis/appmanager/NavigationHelper.java
@@ -8,12 +8,13 @@ public class NavigationHelper extends HelperBase{
         super(app);
     }
     public void managePage() {
-        click(By.xpath("//div[@id='sidebar']/ul/li[6]/a/span"));
+        click(By.xpath("//div[@id='sidebar']/ul/li[7]/a/i"));
 
     }
 
     public void usersManageTab() {
         click(By.linkText("Manage Users"));
+
 
     }
 }
