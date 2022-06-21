4a49db010107fd8300418c83049a907a840cc12c
luminorena <bakhuta.oa@gmail.com>
2022-06-21 13:58:09 +0300

    task 21 Chrome driver, target and code changes

--- addressbook-web-tests/src/test/java/soap/stqa/pft/addressbook/applicationmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/soap/stqa/pft/addressbook/applicationmanager/ApplicationManager.java
@@ -45,9 +45,9 @@ public class ApplicationManager {
 
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format
+                ("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
-        properties.load(new FileReader((new File(String.format
-                ("src/test/resources/%s.properties", target)))));
 
         if ("".equals(properties.getProperty("selenium.server"))) {
             if (Objects.equals(browser, BrowserType.FIREFOX)) {
@@ -58,7 +58,9 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
-            wd = new RemoteWebDriver(new URL("properties.getProperty(\"selenium.server\")"), capabilities);
+            wd = new RemoteWebDriver
+                    (new URL("properties.getProperty(\"selenium.server\")"),
+                            capabilities);
         }
 
 
